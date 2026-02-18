aa22db9ddfd29ebca1dd7cc2e4d98b1441950b30
Armasrc <armandaskontrauskas@gmail.com>
2026-02-18 11:26:15 +0200

    Added VS files folder

---
+++ lab1/ArmandasKontrauskas/Lab1.App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+Random rnd = new Random();
+bool allowChallenge = args.Contains("--challenge");
+int max = allowChallenge ? 5 : 4;
+int task = rnd.Next(1, max + 1);
+
+if(allowChallenge)
+{
+    Console.WriteLine("**Vardas Pavardė:** Armandas Kontrauskas");
+    Console.WriteLine("**Grupė:** PS-5");
+    Console.WriteLine("**Paskaitos data:** 2026-02-11");
+    Console.WriteLine("**Random užduotis:** {0}", task);
+}
+
+
